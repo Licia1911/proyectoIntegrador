4201baf0fd7e8d140e779389acb709d7ca6f08cc
Licia1911 <licia.carreras@gmail.com>
2022-08-03 11:52:43 -0300

    Se  genera routing, Home y Login/ Se crea pantalla de Logeo

---
+++ proyectoIntegrador/backEnd/src/main/java/com/miportfolio/lgc/Security/Enums/RolNombre.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.miportfolio.lgc.Security.Enums;
+
+/**
+ *
+ * @author licia
+ */
+public class RolNombre {
+    
+}
